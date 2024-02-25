1f02e2141605a598f24b2fc3e256c0bd157bdbb4
shayan <shayan.workspace@gmail.com>
2024-02-25 10:59:59 +0500

    fibonacci

--- src/main.v
+++ src/main.v
@@ -1,5 +1,15 @@
 module main
 
+fn fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
 fn main() {
-	println('Hello World!')
+	n := 10
+	result := fibonacci(n)
+	println('The ${n}th Fibonacci number is: ${result}')
 }
